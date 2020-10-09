d1686c1c094d9ece418caf0ebfea52aa086faddb
TotalyDilirios <ogunsina04@gmail.com>
2020-10-09 19:09:34 +0100

    Commited

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let prezi = document.getElementById('prezi')
+
+let width = screen.width
+
+let height = screen.height
+
+prezi.setAttribute('width', width-73)
+prezi.setAttribute('height', height-90)
